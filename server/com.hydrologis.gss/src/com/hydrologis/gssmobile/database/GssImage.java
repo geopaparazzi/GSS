dd0345b208be3a521405c602af68fd8a074ddf18
Andrea Antonello <andrea.antonello@gmail.com>
2018-06-29 15:34:41 +0200

    license update

--- server/com.hydrologis.gss/src/com/hydrologis/gssmobile/database/GssImage.java
+++ server/com.hydrologis.gss/src/com/hydrologis/gssmobile/database/GssImage.java
@@ -1,3 +1,12 @@
+/*******************************************************************************
+ * Copyright (c) 2018 HydroloGIS S.r.l.
+ * 
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ * 
+ * SPDX-License-Identifier: EPL-2.0
+ ******************************************************************************/
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
