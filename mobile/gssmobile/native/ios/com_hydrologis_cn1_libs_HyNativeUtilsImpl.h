310b2d097da2c3d416ecede5444dd9f78f05604a
Andrea Antonello <andrea.antonello@gmail.com>
2020-07-16 12:14:40 +0200

    remove old projects

--- mobile/gssmobile/native/ios/com_hydrologis_cn1_libs_HyNativeUtilsImpl.h
+++
@@ -1,8 +0,0 @@
-#import <Foundation/Foundation.h>
-
-@interface com_hydrologis_cn1_libs_HyNativeUtilsImpl : NSObject {
-}
-
--(BOOL)checkPemissions:(NSString*)param param1:(NSString*)param1;
--(BOOL)isSupported;
-@end
