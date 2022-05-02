5072f21c8e82dd1db374fa8f1bca9a9671ab220f
Francesco Frassinelli <francesco.frassinelli@nina.no>
2022-05-02 16:15:52 +0200

    Define simplified Note model

--- django/gss/data/models.py
+++ django/gss/data/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Note(models.Model):
+    project_name = models.CharField(max_length=100)
+    text = models.CharField(max_length=100)
+    description = models.TextField()
+    ts = models.DateTimeField()
+    lon = models.FloatField()
+    lat = models.FloatField()
+    altim = models.FloatField()
+    form = models.JSONField()
+    marker = models.CharField(max_length=50)
+    size = models.FloatField()
+    rotation = models.FloatField()
+    color = models.CharField(max_length=9)
+    accuracy = models.FloatField()
+    heading = models.FloatField()
+    speed = models.FloatField()
+    speedaccuracy = models.FloatField()
