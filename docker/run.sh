ea0f7ad159ae1c18ef0fc4065fae145b67278fda
Francesco Frassinelli <francesco.frassinelli@nina.no>
2022-11-08 17:23:17 +0100

    Remove old server implementation

--- docker/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -e
-
-WAIT_HOSTS="${POSTGRES_URL%/*}" /wait
-
-args=(-w /workspace)
-[ ! -z "$KEYSTORE" ] && args+=(-s "$KEYSTORE")
-[ ! -z "$KEYSTORE_PASSWORD" ] && args+=(-sp "$KEYSTORE_PASSWORD")
-
-exec java -jar gss-backbone.jar ${args[*]}
--- server_backend_java/scripts/run_simple.sh
+++
@@ -1,4 +0,0 @@
-VERSION=3.3.0
-WORKSPACE=/home/hydrologis/TMP/GSS/
-MEM=-Xmx4g
-java $MEM -jar gss-backbone-$VERSION.jar -w $WORKSPACE -mp testPwd
